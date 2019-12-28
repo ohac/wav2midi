626f4b2702496c15968559e464ad0fa444fa2cec
OHASHI Hideya <ohachige@gmail.com>
2019-12-28 16:06:18 +0900

    save

--- dft.go
+++ dft.go
@@ -37,25 +37,24 @@ func note2str(note int) string {
 	return strtbl[note] + strconv.Itoa(oct)
 }
 
+var basehz = 440.0 / 8 // 55hz
+var tw = 1.05946309    // 12sqrt(2)
+
 func dft(wav []float64, smplfreq float64) []float64 {
 	smpls := len(wav)
-	spct := make([]float64, smpls)
-	for i := 0; i < smpls; i++ {
-		wav[i] *= 0.5 - 0.5*math.Cos(2*math.Pi*float64(i)/float64(smpls))
-	}
-	for i := 0; i < smpls/8; i++ {
-		freq := float64(i) * smplfreq / float64(smpls)
-		if freq2note(freq) == -1 {
-			continue
-		}
+	imin := 7
+	imax := 7 + 12*4 + 1
+	spct := make([]float64, imax-imin)
+	for i := imin; i < imax; i++ {
+		freq := basehz * math.Pow(tw, float64(i))
 		re := 0.0
 		im := 0.0
 		for j := 0; j < smpls; j++ {
-			arg := -2 * math.Pi / float64(smpls) * float64(i*j)
+			arg := -2 * math.Pi * freq / smplfreq * float64(j)
 			re += math.Cos(arg) * wav[j]
 			im += math.Sin(arg) * wav[j]
 		}
-		spct[i] = math.Sqrt(re*re+im*im) / float64(smpls)
+		spct[i-imin] = math.Sqrt(re*re+im*im) / float64(smpls)
 	}
 	return spct
 }
@@ -84,31 +83,31 @@ func readwav(smpls int, fn string) ([]float64, error) {
 
 func eq(freq float64) float64 {
 	if freq < 87 {
-		return 15.0
+		return 20.0
 	} else if freq < 95 {
-		return 5.0
+		return 1.0
 	} else if freq < 150 {
-		return 10.0
+		return 1.0
 	} else if freq < 200 {
 		return 1.0
 	} else if freq < 300 {
-		return 1.0
+		return 1
 	} else if freq < 400 {
-		return 1.0
+		return 1
 	} else if freq < 500 {
-		return 10.0
+		return 1.0
 	} else if freq < 600 {
-		return 10.0
+		return 1.0
 	} else if freq < 700 {
-		return 10.0
+		return 1.0
 	} else if freq < 800 {
-		return 10.0
+		return 1.0
 	} else if freq < 900 {
-		return 10.0
+		return 1.0
 	} else if freq < 1000 {
-		return 10.0
+		return 1.0
 	} else {
-		return 20.0
+		return 1.0
 	}
 }
 
@@ -119,23 +118,52 @@ func main() {
 	smpls := 1024 * 32
 	wav, _ := readwav(smpls, *fn)
 	spct := dft(wav, smplfreq)
+	notes := make([]float64, 128)
 	for i, v := range spct {
-		if i == smpls/8 {
-			break
-		}
+		i2 := i + 7
 		if v > 0.0002 {
-			freq := float64(i) * smplfreq / float64(smpls)
+			freq := basehz * math.Pow(tw, float64(i2))
 			note := freq2note(freq)
 			v *= eq(freq)
-			db := 20 * math.Log10(v)
-			if db > -50 {
-				fmt.Printf("%4d %7.1f Hz %2d %4s %8.6f %6.2f dB ", i, freq, note,
-					note2str(note), v, db)
-				for j := 0; j < (60+int(db))/2; j++ {
-					fmt.Print("*")
+			notes[note] += v
+			/*
+				db := 20 * math.Log10(v)
+				if db > -50 {
+					fmt.Printf("%2d %4s %8.6f %6.2f dB ", note, note2str(note), v, db)
+					for j := 0; j < (60+int(db))/2; j++ {
+						fmt.Print("*")
+					}
+					fmt.Printf("\n")
 				}
-				fmt.Printf("\n")
+			*/
+		}
+	}
+	for note, v := range notes {
+		if note < 90 {
+			notes[note+12] -= v * 1.0
+			if notes[note+12] < 0 {
+				notes[note+12] = 0
 			}
+			notes[note+12+7] -= v * 1.0
+			if notes[note+12+7] < 0 {
+				notes[note+12+7] = 0
+			}
+			notes[note+12+12] -= v * 1.0
+			if notes[note+12+12] < 0 {
+				notes[note+12+12] = 0
+			}
+			notes[note+12+12+7] -= v * 1.0
+			if notes[note+12+12+7] < 0 {
+				notes[note+12+12+7] = 0
+			}
+		}
+		db := 20 * math.Log10(v)
+		if db > -50 {
+			fmt.Printf("%2d %4s %8.6f %6.2f dB ", note, note2str(note), v, db)
+			for j := 0; j < (60+int(db))/2; j++ {
+				fmt.Print("*")
+			}
+			fmt.Printf("\n")
 		}
 	}
 }
