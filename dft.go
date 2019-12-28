20882c3039474448dde85a0a896e04e4e02209d5
OHASHI Hideya <ohachige@gmail.com>
2019-12-28 17:07:12 +0900

    save

--- dft.go
+++ dft.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -25,9 +26,10 @@ var basehz = 440.0 / 8 // 55hz
 var tw = 1.05946309    // 12sqrt(2)
 var imin = 7
 var imax = 7 + 12*4 + 1
+var smplfreq = 44100.0
+var smpls = 1024 * 32
 
-func dft(wav []float64, smplfreq float64) []float64 {
-	smpls := len(wav)
+func dft(wav []float64) []float64 {
 	spct := make([]float64, imax-imin)
 	for i := imin; i < imax; i++ {
 		freq := basehz * math.Pow(tw, float64(i))
@@ -43,26 +45,19 @@ func dft(wav []float64, smplfreq float64) []float64 {
 	return spct
 }
 
-func readwav(smpls int, fn string) ([]float64, error) {
-	wavi := make([]byte, smpls*2)
-	wav := make([]float64, smpls)
-	fp, err := os.Open(fn)
-	if err != nil {
-		return nil, err
-	}
-	defer fp.Close()
+func readwav(fp io.Reader, wavi []byte, wav []float64) error {
 	n, err := io.ReadFull(fp, wavi)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	if n != len(wavi) {
-		return nil, nil
+		return errors.New("")
 	}
 	for i := range wav {
 		wav[i] = float64(int16(wavi[i*2]) | (int16(wavi[i*2+1]) << 8))
 		wav[i] /= 32768.0
 	}
-	return wav, nil
+	return nil
 }
 
 func eq(i int) float64 {
@@ -87,26 +82,49 @@ func reduceharm(spct []float64, i int) {
 	}
 }
 
+func sub(wav []float64, t int) error {
+	spct := dft(wav)
+	for i, v := range spct {
+		v *= eq(i)
+		reduceharm(spct, i)
+	}
+	for i, v := range spct {
+		db := 20 * math.Log10(v)
+		if db > -47 {
+			note := 40 + i
+			fmt.Printf("%2d %2d %4s %8.6f %6.2f dB ", t, note, note2str(note), v, db)
+			for j := 0; j < (60+int(db))/2; j++ {
+				fmt.Print("*")
+			}
+			fmt.Printf("\n")
+		}
+	}
+	return nil
+}
+
 func main() {
 	fn := flag.String("f", "", "filename (.s16)")
 	flag.Parse()
-	smplfreq := 44100.0
-	smpls := 1024 * 32
-	wav, _ := readwav(smpls, *fn)
-	spct := dft(wav, smplfreq)
-	for i, v := range spct {
-		if v > 0.0002 {
-			v *= eq(i)
-			reduceharm(spct, i)
-			db := 20 * math.Log10(v)
-			if db > -50 {
-				note := 40 + i
-				fmt.Printf("%2d %4s %8.6f %6.2f dB ", note, note2str(note), v, db)
-				for j := 0; j < (60+int(db))/2; j++ {
-					fmt.Print("*")
-				}
-				fmt.Printf("\n")
-			}
+	fp, err := os.Open(*fn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer fp.Close()
+	wavi := make([]byte, smpls*2)
+	wav := make([]float64, smpls)
+	shift := 1024
+	for i := 0; ; i++ {
+		var err error
+		if i == 0 {
+			err = readwav(fp, wavi, wav)
+		} else {
+			copy(wav[:smpls-shift], wav[shift:])
+			err = readwav(fp, wavi[:(smpls-shift)*2], wav[smpls-shift:])
 		}
+		if err != nil {
+			break
+		}
+		sub(wav, i)
 	}
 }
