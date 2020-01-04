34405b3362474e81c8a0209d4ce35d6843d67483
OHASHI Hideya <ohachige@gmail.com>
2020-01-04 11:07:21 +0900

    remove dead code

--- dft.go
+++ dft.go
@@ -64,101 +64,6 @@ func readwav(fp io.Reader, wavi []byte, wav []float64, gain float64) error {
 	return nil
 }
 
-/*
-func eq(i int) float64 {
-	switch {
-	case i == 0:
-		return 75.0
-	case i == 1:
-		return 50.0
-	case i == 2:
-		return 30.0
-	case i == 3:
-		return 18.0
-	case i == 4:
-		return 10.0
-	case i == 5:
-		return 15
-	case i == 6:
-		return 10
-	case i == 7:
-		return 10
-	case i == 8:
-		return 16.0
-	case i == 9:
-		return 15.0
-	case i == 10:
-		return 18.0
-	case i == 11:
-		return 4.0
-	case i == 12:
-		return 8.0
-	case i == 13:
-		return 6
-	case i == 14:
-		return 2
-	case i == 15:
-		return 8
-	case i == 16:
-		return 6.0
-	case i == 17:
-		return 10
-	case i == 18:
-		return 4
-	case i == 19:
-		return 3
-	case i == 20:
-		return 4
-	case i == 21:
-		return 0.9
-	case i == 24:
-		return 0.9
-	case i == 28:
-		return 1.5
-	case i == 31:
-		return 4
-	case i == 32:
-		return 20
-	case i == 35:
-		return 6.0
-	default:
-		return 1.0
-	}
-}
-*/
-
-/*
-func reduceharm(spct []float64, i int) {
-	//muls := []float64{1.2, 0.8, 0.5, 0.4, 0.3, 0.2}
-	muls := []float64{0, 0.8, 0.5, 0.4, 0.3, 0.2}
-	for x, j := range []int{12, 12 + 7, 12 + 12, 12 + 12 + 4, 12 + 12 + 7,
-		12 * 3} {
-		k := i + j
-		if k < len(spct) {
-			spct[k] -= spct[i] * muls[x]
-			if spct[k] < 0 {
-				spct[k] = 0
-			}
-		}
-	}
-}
-*/
-
-/*
-func reducenear(spct []float64, i int) {
-	gain := []float64{0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.2, 0.1, 0.05, 0.03}
-	for x, j := range []int{-5, -4, -3, -2, -1, 1, 2, 3, 4, 5} {
-		k := i + j
-		if k >= 0 && k < len(spct) && spct[k] < spct[i] {
-			spct[k] -= spct[i] * gain[x]
-			if spct[k] < 0 {
-				spct[k] = 0
-			}
-		}
-	}
-}
-*/
-
 func guitar(noteon []bool, vels []uint8) {
 	var max uint8
 	maxi := 0
@@ -213,28 +118,14 @@ func sub(wav []float64, t int, delta uint32) (uint32, []bool, []uint8) {
 		wav2[i] *= 0.5 - 0.5*math.Cos(2*math.Pi*float64(i)/float64(smpls))
 	}
 	spct := dft(wav2)
-	/*
-		for i, v := range spct {
-			v *= eq(i)
-			spct[i] = v
-		}
-	*/
-	/* TODO
-	for i := range spct {
-		reducenear(spct, i)
-	}
-	*/
 	noteon := make([]bool, 128)
 	vels := make([]uint8, 128)
 	for i := 0; i < imax-imin-(12*2+4); i++ {
 		v := spct[i]
-		//x1 := spct[i+7]
 		x2 := spct[i+12]
 		x3 := spct[i+12+7]
 		x4 := spct[i+12+12]
 		x5 := spct[i+12+12+4]
-		// TODO
-		//reduceharm(spct, i)
 		db := 20 * math.Log10(v)
 		db2 := 20 * math.Log10(x2)
 		db3 := 20 * math.Log10(x3)
@@ -278,20 +169,6 @@ func sub(wav []float64, t int, delta uint32) (uint32, []bool, []uint8) {
 		}
 	}
 	guitar(noteon, vels)
-	/*
-		for i, v := range noteon {
-			if lastnoteon[i] != v {
-				wr.SetDelta(delta)
-				delta = 0
-				if v {
-					wr.Write(channel.Channel0.NoteOn(uint8(i), vels[i]))
-				} else {
-					wr.Write(channel.Channel0.NoteOff(uint8(i)))
-				}
-			}
-			lastnoteon[i] = v
-		}
-	*/
 	return delta, noteon, vels
 }
 
